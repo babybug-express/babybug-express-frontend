3f7c636ad604bb6c0af9e4f470ae0e6e871b20ee
Simlex <gettosimlex@gmail.com>
2023-07-05 20:29:37 +0100

    Created page

---
+++ src/pages/track-item.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent, ReactElement } from "react";
+
+interface TrackItemProps {
+    
+}
+ 
+const TrackItem: FunctionComponent<TrackItemProps> = ():ReactElement => {
+    return ( 
+        <div>Track item</div>
+     );
+}
+ 
+export default TrackItem;
