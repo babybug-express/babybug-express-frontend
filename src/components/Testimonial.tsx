fe8612789b634a4a22abbc0d906f838199722ee3
Simlex <gettosimlex@gmail.com>
2023-07-05 20:26:56 +0100

    Moved files

--- src/components/Testimonial.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FunctionComponent } from "react";
-import styles from "../styles/Team.module.scss";
-interface TestimonialProps {}
-
-const Testimonial: FunctionComponent<TestimonialProps> = () => {
-  return <div>Testimonial</div>;
-};
-
-export default Testimonial;
